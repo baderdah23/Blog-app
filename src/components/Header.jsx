55984ff254e54329fc45c36b714225d7a38236af
baderdah23 <bd1911.k5@gmail.com>
2026-08-16 14:19:41 +0300

    improve responsive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Search from "./Search";
 
 const Header = ({ setSearchValue }) => {
   return (
-    <header className="mx- mb-12 flex w-100 items-center gap-10 px-5 py-8 md:w-170 md:justify-between lg:w-280">
+    <header className="mx-auto mb-12 flex w-100 items-center justify-center gap-10 px-5 py-8 md:w-170 md:justify-between lg:w-280">
       <Link to="/" className="flex cursor-pointer gap-3 lg:gap-7">
         <svg
           width="36"
